535c5f76c1d61879a206cb6d6b85bf3312fc4daf
patwicks <it.patrickjohngsantillan@gmail.com>
2022-08-05 20:40:33 +0800

    add changes

--- emailer/email.routes.js
+++ emailer/email.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const { SEND_MAIL, HELLO } = require("./email.controller");
 
-// router.post("/email", SEND_MAIL);
+router.post("/email", SEND_MAIL);
 router.get("/hello", HELLO);
 
 module.exports = router;
