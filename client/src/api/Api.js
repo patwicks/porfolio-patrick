6ec817d2e1253eae0e5190799910e2fd05cea1c1
patwicks <it.patrickjohngsantillan@gmail.com>
2022-08-05 22:08:41 +0800

    add changes

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dev-test-emailer.herokuapp.com/v1/api/send",
+  baseURL: "https://dev-pat.herokuapp.com/v1/api/send",
   // baseURL: "http://localhost:3000/v1/api/send", //Local testing
 });
 
